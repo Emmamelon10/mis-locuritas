0c0c23ca2ced551f0a2d669cbfd7a6f480e822e6
Emmamelon10 <131699411+Emmamelon10@users.noreply.github.com>
2023-06-13 01:28:41 -0300

    Add files via upload

---
+++ Ventanita/Ventanita/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Ventanita.rc
+//
+#define DLG_1                           101
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     104
+#define IDB_BITMAP3                     105
+#define IDB_BITMAP4                     106
+#define IDC_SCROLLBAR1                  1001
+#define SCR_1                           1001
+#define PIC_1                           1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
