a596db71a87c3ee567b0c909cdbec344b7c783a8
Emmamelon10 <131699411+Emmamelon10@users.noreply.github.com>
2023-06-14 09:57:58 -0300

    Update Source.cpp

--- Ventanita/Ventanita/Source.cpp
+++ Ventanita/Ventanita/Source.cpp
@@ -1,16 +1,18 @@
 #include <Windows.h>
-#include "resource.h"
+#include "resource.h" //PROGRAMA DE RECURSOS
 
-BOOL CALLBACK fVentana1(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK fVentana1(HWND, UINT, WPARAM, LPARAM);   //ESTABLECER LAS PROPIEDADES DE LA VENTANA
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int cShow) {
-	HWND hVentana1 = CreateDialog(hInst, MAKEINTRESOURCE(DLG_1), NULL, fVentana1);
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int cShow)
+{
+	HWND hVentana1 = CreateDialog(hInst, MAKEINTRESOURCE(DLG_1), NULL, fVentana1);  //PROGRAMA PARA INCLUIR LA VENTANA Y SUS ATRIBUTOS
 	ShowWindow(hVentana1, cShow);
 
-	MSG msg;
-	ZeroMemory(&msg, sizeof(MSG));
+	MSG msg; 							//ESTABLECER UNA VARIABLE PARA ELCOMANDO DEL USUSARIO
+	ZeroMemory(&msg, sizeof(MSG));		//LIMPIAR LA MEMORIA POR SI QUEDA BASURA 
 
-	while (GetMessage(&msg, NULL, NULL, NULL)) {
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -18,12 +20,30 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int cShow) {
 }
 
 
-BOOL CALLBACK fVentana1(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	switch (msg) {
-	case WM_CLOSE:
+BOOL CALLBACK fVentana1(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) //ESTABLECER EL CUERPO DE LA VENTANA
+{
+	switch (msg) 
+	{
+	case WM_COMMAND:
+		{
+			//HIWORD/*QUE*/   LOWORD/*QUIEN*/
+			if(LOWORD(wparam) == BTN_2 && HIWORD(wparam) == BN_CLICKED) //SI LA ORDEN RECIBIDA EN EL BOTON 1 ES "APRETADO"
+			{
+				MessageBox(NULL,"*Una ventanita aparece amistosamente a tu pantalla* :D","Ventanita :)",MB_OK);
+			}
+			if(LOWORD(wparam) == BTN_1 && HIWORD(wparam) == BN_CLICKED)
+			{
+				MessageBox(hwnd,"AHHHHH NOO AHORA ME VOY A MORIR D:","Bruh",MB_OK);
+				MessageBox(hwnd,"    x_x *SE MUERE*","*Muerte dramatica*",MB_OK);
+				DestroyWindow(hwnd);
+			}
+		}
+		
+		break;
+	case WM_CLOSE:            //CASO APRIETE LA X, CIERRE LA VENTANA
 		DestroyWindow(hwnd);
 		break;
-	case WM_DESTROY:
+	case WM_DESTROY:        //CASO CIERRE LA VENTANA, CIERRE EL PROGRAMA
 		PostQuitMessage(117);
 		break;
 	}
