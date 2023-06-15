6b24f88c887a85132caea52123543328f1223229
Emmamelon10 <131699411+Emmamelon10@users.noreply.github.com>
2023-06-15 00:36:39 -0300

    Update Ventanita

--- Ventanita/Ventanita/Source.cpp
+++ Ventanita/Ventanita/Source.cpp
@@ -22,24 +22,33 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR cmdLine, int cShow)
 
 BOOL CALLBACK fVentana1(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) //ESTABLECER EL CUERPO DE LA VENTANA
 {
-	switch (msg) 
+	switch (msg)
 	{
 	case WM_COMMAND:
+	{
+		//HIWORD/*QUE*/   LOWORD/*QUIEN*/
+		if (LOWORD(wparam) == BTN_2 && HIWORD(wparam) == BN_CLICKED) //SI LA ORDEN RECIBIDA EN EL BOTON 1 ES "APRETADO"
 		{
-			//HIWORD/*QUE*/   LOWORD/*QUIEN*/
-			if(LOWORD(wparam) == BTN_2 && HIWORD(wparam) == BN_CLICKED) //SI LA ORDEN RECIBIDA EN EL BOTON 1 ES "APRETADO"
+			//MessageBox(NULL,(LPWSTR) ":ESTOY VIVA :D",(LPCWSTR) "Ventanita", MB_OK);
+			HWND Textoscan = GetDlgItem(hwnd, IDC_EDIT1);
+			int Longitud = GetWindowTextLength((HWND)IDC_EDIT1);
+			char texto[100];
+			GetWindowText(Textoscan, (LPWSTR)texto, Longitud+1);
+			MessageBox(NULL, (LPCWSTR)texto, (LPCWSTR)"tu texto campeón :)", MB_OK);
+			if (Longitud < 1)
 			{
-				MessageBox(NULL,"*Una ventanita aparece amistosamente a tu pantalla* :D","Ventanita :)",MB_OK);
-			}
-			if(LOWORD(wparam) == BTN_1 && HIWORD(wparam) == BN_CLICKED)
-			{
-				MessageBox(hwnd,"AHHHHH NOO AHORA ME VOY A MORIR D:","Bruh",MB_OK);
-				MessageBox(hwnd,"    x_x *SE MUERE*","*Muerte dramatica*",MB_OK);
-				DestroyWindow(hwnd);
+				MessageBox(hwnd, (LPCWSTR)"Ah sos graciosito vos, dale crack, poné algo en el textito :)", (LPCWSTR)"jajaja",MB_OK);
 			}
 		}
-		
-		break;
+		if (LOWORD(wparam) == BTN_1 && HIWORD(wparam) == BN_CLICKED)
+		{
+			MessageBox(hwnd,(LPWSTR) "AHHHHH NOO AHORA ME VOY A MORIR D:",(LPWSTR) "Bruh", MB_OK);
+			MessageBox(hwnd,(LPWSTR) "x_x *SE MUERE*", (LPWSTR)"*Muerte dramatica*", MB_OK);
+			DestroyWindow(hwnd);
+		}
+	}
+
+	break;
 	case WM_CLOSE:            //CASO APRIETE LA X, CIERRE LA VENTANA
 		DestroyWindow(hwnd);
 		break;
--- Ventanita/Ventanita/resource.h
+++ Ventanita/Ventanita/resource.h
@@ -4,20 +4,19 @@
 //
 #define DLG_1                           101
 #define IDB_BITMAP1                     103
-#define IDB_BITMAP2                     104
-#define IDB_BITMAP3                     105
-#define IDB_BITMAP4                     106
-#define IDC_SCROLLBAR1                  1001
-#define SCR_1                           1001
-#define PIC_1                           1002
+#define BTN_1                           1001
+#define BTN_2                           1002
+#define IDC_EDIT1                       1003
+#define IMG_1                           1004
+#define IDC_BUTTON1                     1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
